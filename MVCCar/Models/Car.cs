a510d94cd321749ee880e934f0743d5169464242
Melita Župarić <meli.zuparic@gmail.com>
2025-11-19 18:35:03 +0100

    Melita Župarić Lab3

---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Color { get; set; }
+        public int ProductionYear { get; set; }
+        public decimal Price { get; set; }
+        public required string Manufacturer { get; set; }
+    }
+}
